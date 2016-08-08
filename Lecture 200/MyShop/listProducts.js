a84c1126c6280423db0dac7b39c9b2325b03264f
Jeanne Sneed <jsneed@gmail.com>
2016-08-08 16:58:08 -0700

    NPM Faker library

---
+++ Lecture 200/MyShop/listProducts.js
@@ -0,0 +1,9 @@
+var faker = require("faker");
+
+console.log("Products\n--------------------------");
+for(var i = 0; i < 10; i++)
+{
+    var randomProduct = faker.commerce.productName();
+    var randomPrice = faker.commerce.price();
+    console.log(randomProduct + " - $" + randomPrice);
+}
