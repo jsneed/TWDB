34e7ef047425a3c96e146a2b26e5ce63eb40f8c1
Jeanne Sneed <jsneed@gmail.com>
2016-08-08 18:10:04 -0700

    update

--- Lecture 202/FirstExpressApp/app.js
+++ Lecture 202/FirstExpressApp/app.js
@@ -1,14 +1,20 @@
 var express = require("express");
 var app = express();
 
-
 app.get("/", function(req, res){
     res.send("Hi there!");
 });
 
+app.get("/bye", function(req, res){
+    res.send("Goodbye!");
+});
+
+app.get("/dog", function(req, res){
+    res.send("Roof!");
+});
 
 //Tell Express to listen for requests (start server)
 //Need to user process.env.* for Cloud9 IDE
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, function(){
     console.log("Server has started on port: " + process.env.PORT);
 });
