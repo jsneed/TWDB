e134a157696d9ef5e0d8075e9b8d7a512f9adf25
Jeanne Sneed <jsneed@gmail.com>
2016-08-17 16:45:34 -0700

    remove log statement

--- Lecture 261/YelpCamp/routes/index.js
+++ Lecture 261/YelpCamp/routes/index.js
@@ -22,7 +22,7 @@ router.post("/register", function(req, res){
     var newUser = new User({username : req.body.username});
     User.register(newUser, req.body.password, function(err, user){
         if(err) {
-            console.log(err);
+            //console.log(err);
             req.flash("error", err.message);
             res.redirect("back");
         }
